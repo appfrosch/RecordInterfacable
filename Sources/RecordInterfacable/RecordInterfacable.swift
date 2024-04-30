ac67db0f8684aaa017a809dcc1eaa23a628046b4
Appfrosch <splash@appfros.ch>
2024-04-30 08:03:09 +0200

    adds comment to describe behaviour of macro

--- Sources/RecordInterfacable/RecordInterfacable.swift
+++ Sources/RecordInterfacable/RecordInterfacable.swift
@@ -1,11 +1,61 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
+/// A macro that produces a containing `struct` with all the properties
+/// in a `class` to be able to conform to `Codable` even for classes that
+/// interfere with `Codable` because they conform to `@Observable`.
 ///
-///     #stringify(x + y)
+/// Classes conforming to `@Observable` will automatically produce `CodingKeys` for
+/// the `_`-properties this conformance produces, which in turn will mess with
+/// packages that rely on `Codable`.
 ///
-/// produces a tuple `(x + y, "x + y")`.
+/// This macro is adding a `struct` to such a `class` that can be used for `Codable`
+/// instead of the `class` itself that contains.
+///
+/// ```swift
+/// @Observable
+/// @RecordInterfacable
+/// class Model {
+///   let id: UUID
+///   var title: String
+///   init(
+///     id: UUID = UUID(),
+///     title: String
+///   ) {
+///     self.id = id
+///     self.title = title
+///   }
+/// }
+/// ```
+/// becomes
+/// ```swift
+/// @Observable
+/// class Model {
+///   let id: UUID
+///   var title: String
+///   init(
+///     id: UUID = UUID(),
+///     title: String
+///   ) {
+///     self.id = id
+///     self.title = title
+///   }
+///
+///   struct ModelRecord: Codable {
+///     let id: UUID
+///     var title: String
+///   }
+///
+///   init(from record: ModelRecord) {
+///     init(
+///       id: record.id,
+///       title: record.title
+///     )
+///   }
+///
+///   func convertToRecord() -> ModelRecord {
+///     ModelRecord(
+///       id: self.id,
+///       title: self.title
+///     )
+/// }
+/// ```
 @freestanding(expression)
 public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "RecordInterfacableMacros", type: "StringifyMacro")
